98d45651524128f7ab4bcc108e60ab925d3f2c9e
Marani Matias Ezequiel <maranimatias@gmail.com>
2015-04-15 14:51:50 -0300

    test ok, y nuevo empleado anda

--- routes/main.js
+++ routes/main.js
@@ -58,11 +58,10 @@ app.get('/panel/employees/new', adminAuth, function(req, res){
     res.render('new', { title: 'New Employees', url: '/panel/employees/new' });
 });
 app.post('/panel/employees/new', adminAuth, function(req, res){
-  emp = new Employees({ name: req.body.name, surname: req.body.surname, email: req.body.email, password: req.body.password});
+  var emp = new Employees({ name: req.body.name, surname: req.body.surname, email: req.body.email, password: req.body.password});
   emp.save(function(err, doc){
     if(!err){
       res.redirect('/panel/employees');
-      done();
     }else{
       res.redirect('/error');   
     }
