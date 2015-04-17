e8924d28823911f4b62d7b778c417fff0233fe30
Marani Matias Ezequiel <maranimatias@gmail.com>
2015-04-16 21:57:41 -0300

    test compactado

--- test/test.js
+++ test/test.js
@@ -16,82 +16,52 @@ describe('Conexion a MongoDB.', function(){
 });
 
 describe('Validad contraseña.', function(){
+  before(function() {
+    employees = new Employees({name : 'empleadoUNO' , 
+                               surname : 'surnameTest' , 
+                               provider : 'test',
+                               email : 'test@test.com' , 
+                               password : 'qwe' });
+  })
   describe('- Contaseña es incorecta.', function(){
     it('-> qwe', function(){
-      employees = new Employees({name : 'empleadoUNO' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'test@test.com' , 
-                                 password : 'qwe' });
-
+      employees.password =  'qwe';
       employees.save(function(err){
          should(err).be.not.empty;
       });
     });
     it('-> qwertyyyy', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : 'qwertyyyy' });
-        employees.save(function(err){
-         should(err).be.not.empty;
-        });
-
+      employees.password = 'qwertyyyy';
+      employees.save(function(err){
+        should(err).be.not.empty;
+      });
     });
     it('-> querty1213', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : 'querty1213' });
+        employees.password = 'querty1213';
         employees.save(function(err){
          should(err).be.not.empty;
         });
-
     });
     it('-> 123456789', function(){
-
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : '123456789' });
+        employees.password = '123456789';
         employees.save(function(err){
          should(err).be.not.empty;
         });
-
     });
     it('-> 1234567@', function(){
-    
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : '1234567@' });
+        employees.password = '1234567@' ;
         employees.save(function(err){
             should(err).be.not.empty;
         });
-
     });
     it('-> Qwerty123' , function(){
-  
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : 'Qwerty123' });
+        employees.password = 'Qwerty123';
         employees.save(function(err){
             should(err).be.not.empty;
         });
- 
     });
     it('-> @qwerty123', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : '@qwerty123' });
+        employees.password = '@qwerty123';
         employees.save(function(err){
             should(err).be.not.empty;
         });
@@ -99,11 +69,7 @@ describe('Validad contraseña.', function(){
   });
     describe('- Contraseña es correcta.', function(){
       it('-> @Qwerty123', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'test@test.com' , 
-                                   password : '@Qwerty123' });
+        employees.password = '@Qwerty123';
         employees.save(function(err){
             should(err).be.empty;
         });
@@ -134,61 +100,37 @@ describe('Validad email.', function(){
         });
     });
     it('-> admin@admin', function(){
-       employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin@admin' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'admin@admin';
         employees.save(function(err){
-            should(err).be.not.empty;
+          should(err).be.not.empty;
         });
     });
     it('-> admin.com', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin.com' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'admin.com';
         employees.save(function(err){
             should(err).be.not.empty;
         });
     });
     it('-> admin', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'admin';
         employees.save(function(err){
             should(err).be.not.empty;
         });
     });
     it('-> admin@', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin@' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'admin@';
         employees.save(function(err){
             should(err).be.not.empty;
         });
     });
     it('-> admin@admin@admin.com' , function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin@admin@admin.com' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'admin@admin@admin.com' ;
         employees.save(function(err){
             should(err).be.not.empty;
         });
     });
     it('-> a@admin.com.admin@admin', function(){
-        employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'a@admin.com.admin@admin' , 
-                                   password : '@Qwerty123' });
+        employees.email = 'a@admin.com.admin@admin' ;
         employees.save(function(err){
             should(err).be.not.empty;
         });
@@ -196,21 +138,13 @@ describe('Validad email.', function(){
   });
   describe('- Email es correcta.', function(){
     it('-> admin@admin.com.ar', function(){
-      employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'admin@admin.com.ar' , 
-                                   password : '@Qwerty123' });
+      employees.email = 'admin@admin.com.ar';
       employees.save(function(err){
           should(err).be.empty;
       });
     });
     it('-> matias@admin.com', function(){
-      employees = new Employees({name : 'empleadoUNO' , 
-                                   surname : 'surnameTest' , 
-                                   provider : 'test',
-                                   email : 'matias@admin.com' , 
-                                   password : '@Qwerty123' });
+      employees.email = 'matias@admin.com';
       employees.save(function(err){
           should(err).be.empty;
       });
@@ -241,41 +175,25 @@ describe('Validad nombre.', function(){
       });
     });
     it('-> nobre1', function(){
-      employees = new Employees({name : 'nobre1' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.name = 'nobre1';
       employees.save(function(err){
           should(err).be.not.empty;
       });
     });
     it('-> a', function(){
-      employees = new Employees({name : 'a' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.name = 'a' ;
       employees.save(function(err){
           should(err).be.not.empty;
       });
     });
     it('-> 1nombre', function(){
-      employees = new Employees({name : '1nombre' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.name = '1nombre';
       employees.save(function(err){
           should(err).be.not.empty;
       });
     });
     it('-> nombre@', function(){
-      employees = new Employees({name : 'nombre@' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.name = 'nombre@' ;
       employees.save(function(err){
           should(err).be.not.empty;
       });
@@ -283,11 +201,7 @@ describe('Validad nombre.', function(){
   });
   describe('- Nombre es correcta.', function(done){
     it('-> matias', function(){
-      employees = new Employees({name : 'matias' , 
-                                 surname : 'surnameTest' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.name = 'matias' ;
       employees.save(function(err){
           should(err).be.empty;
       });
@@ -298,41 +212,25 @@ describe('Validad nombre.', function(){
 describe('Validad apellido.', function(done){
   describe('- Apellido es incorecta.', function(done){
     it('-> nobre1', function(){
-        employees = new Employees({name : 'matias' , 
-                                   surname : 'nobre1' , 
-                                   provider : 'test',
-                                   email : 'admin@admin.com' , 
-                                   password : '@Qwerty123' });
+        employees.surname = 'nobre1';
       employees.save(function(err){
           should(err).be.not.empty;
       });
     });
     it('-> a', function(){
-        employees = new Employees({name : 'matias' , 
-                                   surname : 'a' , 
-                                   provider : 'test',
-                                   email : 'admin@admin.com' , 
-                                   password : '@Qwerty123' });
+        employees.surname = 'a' ;
       employees.save(function(err){
           should(err).be.not.empty;
       });
     });
     it('-> 1nombre', function(){
-        employees = new Employees({name : 'matias' , 
-                                   surname : '1nombre' , 
-                                   provider : 'test',
-                                   email : 'admin@admin.com' , 
-                                   password : '@Qwerty123' });
-      employees.save(function(err){
-          should(err).be.not.empty;
-      });
+        employees.surname = '1nombre';
+        employees.save(function(err){
+            should(err).be.not.empty;
+        });
     });
     it('-> nombre@', function(){
-        employees = new Employees({name : 'matias' , 
-                                   surname : 'nombre@' , 
-                                   provider : 'test',
-                                   email : 'admin@admin.com' , 
-                                   password : '@Qwerty123' });
+        employees.surname = 'nombre@' ;
       employees.save(function(err){
           should(err).be.not.empty;
       });
@@ -340,11 +238,7 @@ describe('Validad apellido.', function(done){
   });
   describe('- Apellido es correcta.', function(done){
     it('-> matias', function(){
-      employees = new Employees({name : 'matias' , 
-                                 surname : 'matias' , 
-                                 provider : 'test',
-                                 email : 'admin@admin.com' , 
-                                 password : '@Qwerty123' });
+      employees.surname='matias' ;
       employees.save(function(err){
           should(err).be.not.empty;
       });
